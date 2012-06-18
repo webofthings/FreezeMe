44e067de0771a4824f8d50db156f07a5ff364697
Aitor <tulvur@vimur.(none)>
2012-06-18 16:51:16 +0100

    The data is not longer loaded on the main Thread (otherwise, android throws the new NetworkOnMainThreadException runtime exception).

--- src/org/webofthings/freezeme/Visualizer.java
+++ src/org/webofthings/freezeme/Visualizer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.webofthings.freezeme.dao.AbstractFoodDAO;
 import org.webofthings.freezeme.dao.FakeFoodDAO;
@@ -21,6 +23,7 @@ import android.widget.TextView;
 
 public class Visualizer extends Activity {
 	String urlToAccessMoreInfo = "http://";
+	final ExecutorService ex = Executors.newSingleThreadExecutor();
 	
     /** Called when the activity is first created. */
     @Override
@@ -33,27 +36,7 @@ public class Visualizer extends Activity {
 		
 		Log.d(AppData.log, uri);
 		
-		final AbstractFoodDAO food = new FakeFoodDAO();
-		food.load();
-        
-        //final Bundle receiveBundle = this.getIntent().getExtras();
-        this.urlToAccessMoreInfo = food.getInfoUrl();
-        
-        final TextView lblName = (TextView) findViewById(R.id.textView1);
-        lblName.setText(food.getName());
-        
-        final TextView lblDaysLeft = (TextView) findViewById(R.id.textView2);
-        lblDaysLeft.setText(food.getExpiration());
-        
-        try {
-    	  ImageView i = (ImageView)findViewById(R.id.imageView1);
-    	  Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(food.getImageUrl()).getContent());
-    	  i.setImageBitmap(bitmap); 
-    	} catch (MalformedURLException e) {
-    		Log.d(AppData.log, e.getMessage());
-    	} catch (IOException e) {
-    		Log.d(AppData.log, e.getMessage());
-    	}
+		this.ex.submit(new DataLoader(new FakeFoodDAO())); // Ignore the Future<>
     }
     
     public void goToURL(View v) {
@@ -64,5 +47,38 @@ public class Visualizer extends Activity {
 	@Override
 	public void onResume() {
 		super.onResume();
+	}
+	
+	class DataLoader implements Runnable {
+		AbstractFoodDAO dao;
+		
+		public DataLoader(AbstractFoodDAO dao) {
+			this.dao = dao;
+		}
+
+		@Override
+		public void run() {
+			this.dao.load();
+			
+	        //final Bundle receiveBundle = this.getIntent().getExtras();
+			Visualizer.this.urlToAccessMoreInfo = this.dao.getInfoUrl();
+	        
+	        final TextView lblName = (TextView) findViewById(R.id.textView1);
+	        lblName.setText(this.dao.getName());
+	        
+	        final TextView lblDaysLeft = (TextView) findViewById(R.id.textView2);
+	        lblDaysLeft.setText(this.dao.getExpiration());
+	        
+	        try {
+	    	  ImageView i = (ImageView)findViewById(R.id.imageView1);
+	    	  Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(this.dao.getImageUrl()).getContent());
+	    	  i.setImageBitmap(bitmap); 
+	    	} catch (MalformedURLException e) {
+	    		Log.d(AppData.log, e.getMessage());
+	    	} catch (IOException e) {
+	    		Log.d(AppData.log, "Something went wrong loading the image");
+	    		Log.d(AppData.log, e.getMessage());
+	    	}
+		}
 	}
 }
